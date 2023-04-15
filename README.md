671b35176116c549dac6a989a74291efbe9ac936
ostwilkens <carl@ostwilkens.se>
2023-04-15 23:00:15 +0200

    WIP

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # mARC
 
-A [matrix](https://matrix.org/) client daemon utilizing static site generation. Inspired by the [mind-blowing speed](https://en.wikipedia.org/wiki/Brown-throated_sloth) and [simplicity](https://github.com/vector-im/element-web/issues) of [Element](https://github.com/vector-im/element-web/issues). 
+A [matrix](https://matrix.org/) client daemon utilizing append-only static site generation. Inspired by the [mind-blowing speed](https://en.wikipedia.org/wiki/Brown-throated_sloth) and [simplicity](https://github.com/vector-im/element-web/issues) of [Element](https://github.com/vector-im/element-web/issues). 
 
 ## Usage
 - Rename `.env.example` to `.env` and update the values
@@ -9,18 +9,25 @@ A [matrix](https://matrix.org/) client daemon utilizing static site generation. 
 - Serve generated files with a http server of choice: `cd www && npx serve`
 
 ## Planned features
+- Avoid duplication of logic in python + js
 - ~~Combined room+people list~~
 - ~~One statically generated html file per room~~
-- Usable search
-- IRC-convention "edit history" (*fix)
+- ~~Use http api instead of simplematrixbotlib~~
+- ~~Fetch history~~
+- ~~/sync~~
+- ~~Message sending~~
+- ~~IRC-convention "edit history" (*fix)~~
 - Flag but never delete messages
 - Require browser cert when accessing the site and sending messages
-- Blocking http request waiting for room.html updates
-- Message sending
+- ~~Blocking http request waiting for room.html updates~~
 - Script injection protection
+- Usable search
+- Hover to show image
+- Easy setup
 
 ## Undecided on
 - Reactions
+- Typing status
 
 ## Features I'm looking forward to not implementing
 - Daily notification that tells you to restart the client for an update that does nothing
@@ -28,6 +35,11 @@ A [matrix](https://matrix.org/) client daemon utilizing static site generation. 
 - Threads
 - Video or VoIP
 - Historical revisionism (message deletion)
+- Stickers
 
 ## Current state
 ![Screenshot](scrot.png)
+
+## Resources
+https://spec.matrix.org/v1.6/client-server-api/#get_matrixclientv3sync
+https://gist.github.com/RickCogley/69f430d4418ae5498e8febab44d241c9
--- mitm.py
+++ mitm.py
@@ -1,68 +1,167 @@
 #%%
-import simplematrixbotlib as botlib
 import os
 import time
+import requests
+import json
+import sqlite3
+import chevron
 from dotenv import load_dotenv
+
 load_dotenv(".env")
 MATRIX_SERVER_URL = os.getenv("MATRIX_SERVER_URL")
-MATRIX_USERNAME = os.getenv("MATRIX_USERNAME")
-MATRIX_PASSWORD = os.getenv("MATRIX_PASSWORD")
-
-creds = botlib.Creds(MATRIX_SERVER_URL, MATRIX_USERNAME, MATRIX_PASSWORD)
-bot = botlib.Bot(creds)
-
-db = { "messages": {} }
-
-open("www/room_list.html", "w").write(open("www/room_list.template.html", "r").read())
+MATRIX_ACCESS_TOKEN = os.getenv("MATRIX_ACCESS_TOKEN")
 
 
-@bot.listener.on_startup
-async def on_startup(room_id):
-    room = bot.async_client.rooms[room_id]
-    print(f"Startup: joined room {room_id}, user count: {len(room.users)}")
-
-    if len(room.users) <= 1:
-        return
-
-    room.display_name
-    file_name = room_id.replace("!", "_").replace(":", "_").replace(".", "-")
-    room_link = f"<a target='_parent' href='/?room={file_name}'>{room.display_name}</a>"
-
-    with open(f"www/room_list.html", "a") as f:
-        f.write(f"{room_link}<br>")
-
-    with open(f"www/rooms/{file_name}.html", "a+") as f:
-        is_empty = f.tell() == 0
-        if is_empty:
-            f.write(open("www/room.template.html", "r").read())
+template = open("template.html", "r").read()
+db = sqlite3.connect("db.sqlite")
+cursor = db.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS messages (event_id text PRIMARY KEY, room_id text, sender text, sender_display_name text, timestamp integer, body text)")
+cursor.execute("CREATE TABLE IF NOT EXISTS rooms (room_id text PRIMARY KEY, display_name text NULL)")
+cursor.execute("CREATE TABLE IF NOT EXISTS store (key text PRIMARY KEY, value text)")
+cursor.execute("CREATE TABLE IF NOT EXISTS members (room_id text, user_id text, displayname text, PRIMARY KEY (room_id, user_id))")
+db.commit()
 
 
+def render(timestamp, only_room_id=None):
+    my_user_id = cursor.execute("SELECT value FROM store WHERE key = 'my_user_id'").fetchone()[0]
 
-@bot.listener.on_message_event
-async def on_message_event(room, message):
-    print("------------ on_message ------------")
-    room_id = room.room_id # !rYxxSjtAOKXDifduyW:matrix.znurre.com
-    # room_name = room.display_name
-    sender = message.sender # '@xinux:matrix.znurre.com'
-    body = message.body
-    # event_id = message.source["event_id"] # '$lKT6skTe'
-    # content = message.source["content"]
-    # is_edit = "m.new_content" in content
-    # new_content = content["m.new_content"] if is_edit else None
-    # relates_to = content["m.relates_to"] if is_edit else None
-    # msgtype = content["msgtype"] # 'm.text'
+    cursor.execute("SELECT DISTINCT rooms.room_id, rooms.display_name, first_room_member_displayname, member_count FROM rooms LEFT JOIN (SELECT room_id, displayname AS first_room_member_displayname FROM members WHERE user_id != ? GROUP BY room_id) AS hehu ON rooms.room_id = hehu.room_id LEFT JOIN (SELECT room_id, COUNT(*) AS member_count FROM members GROUP BY room_id) AS hehu2 ON rooms.room_id = hehu2.room_id ORDER BY rooms.display_name ASC",
+                   (my_user_id,))
+    rooms = [{ 
+        "room_id": x[0], 
+        "filename": x[0].replace("!", "_").replace(":", "_").replace(".", "-"),
+        "display_name": x[1] or x[2] or x[0],
+        "member_count": x[3]
+    } for x in cursor.fetchall()]
 
-    sender_display_name = sender[1:].split(":")[0]
-    unix_timestamp = message.source["origin_server_ts"]
-    hhmm = time.strftime("%H:%M", time.localtime(unix_timestamp/1000))
-    text = f"<span>[{hhmm}] &lt;{sender_display_name}&gt; {body}</span>"
+    for room in rooms:
+        if only_room_id and room["room_id"] != only_room_id:
+            continue
 
-    # clean up room_id so it can be used as a filename
-    file_name = room_id.replace("!", "_").replace(":", "_").replace(".", "-")
+        cursor.execute("SELECT sender, sender_display_name, timestamp, body FROM messages WHERE room_id = ? ORDER BY timestamp ASC", (room["room_id"],))
+        messages = [{
+            "sender": x[0], 
+            "sender_display_name": x[1], 
+            "timestamp": x[2],
+            "body": x[3],
+            "hhmm": time.strftime("%H:%M", time.localtime(x[2]/1000)),
+        } for x in cursor.fetchall()]
 
-    # append message to roomid.html
-    open(f"www/rooms/{file_name}.html", "a").write(f"{text}<br>")
+        data = {
+            "rooms": rooms,
+            "messages": messages,
+            "room_id": room["room_id"],
+            "room_display_name": room["display_name"],
+            "access_token": MATRIX_ACCESS_TOKEN,
+            "build_batch_ts": timestamp
+        }
+
+        html = chevron.render(template, data)
+        open(f"www/{room['filename']}.html", "w").write(html)
 
 
-await bot.main()
+def fetch_whoami():
+    url = f"https://matrix.znurre.com/_matrix/client/r0/account/whoami?access_token={MATRIX_ACCESS_TOKEN}"
+    response = requests.get(url)
+    data = json.loads(response.content)
+    cursor.execute("INSERT OR REPLACE INTO store VALUES (?, ?)", ("my_user_id", data["user_id"]))
 
+
+def fetch_rooms(since=None):
+    url = f"https://matrix.znurre.com/_matrix/client/r0/sync?filter={{\"room\":{{\"timeline\":{{\"limit\":1}}}}}}&access_token={MATRIX_ACCESS_TOKEN}"
+
+    if since:
+        url += f"&since={since}"
+
+    response = requests.get(url)
+    data = json.loads(response.content)
+
+    if "account_data" in data:
+        for event in data["account_data"]["events"]:
+            if event["type"] == "m.direct":
+                content = event["content"]
+                for user_id, room_ids in content.items():
+                    for room_id in room_ids:
+                        # add user as member to room
+                        cursor.execute("INSERT OR IGNORE INTO members VALUES (?, ?, NULL)", (room_id, user_id))
+
+    if "rooms" in data:
+        for room_id, room in data["rooms"]["join"].items():
+            
+            cursor.execute("INSERT OR IGNORE INTO rooms VALUES (?, NULL)", (room_id,))
+
+            for event in room["state"]["events"]:
+
+                if event["type"] == "m.room.member":
+                    content = event["content"]
+
+                    if content["membership"] == "join":
+                        displayname = content.get("displayname") or event["sender"][1:].split(":")[0]
+                        cursor.execute("INSERT OR IGNORE INTO members VALUES (?, ?, ?)", (room_id, event["sender"], displayname))
+                        
+                        # update displayname of member
+                        cursor.execute("UPDATE members SET displayname = ? WHERE user_id = ?", (displayname, event["sender"]))
+
+                    elif content["membership"] == "leave":
+                        cursor.execute("DELETE FROM members WHERE room_id = ? AND user_id = ?", (room_id, event["sender"]))
+
+                if event["type"] == "m.room.name":
+                    room_name = event["content"]["name"]
+
+                    # update name
+                    cursor.execute("UPDATE rooms SET display_name = ? WHERE room_id = ?", (room_name, room_id))
+                    
+    db.commit()
+
+
+def fetch_events(since=None):
+    url = f"https://matrix.znurre.com/_matrix/client/r0/sync?timeout=30000&access_token={MATRIX_ACCESS_TOKEN}"
+
+    if since:
+        url += f"&since={since}"
+
+    response = requests.get(url)
+    data = json.loads(response.content)
+
+    next_batch = data["next_batch"]
+    cursor.execute("INSERT OR REPLACE INTO store VALUES (?, ?)", ("next_batch", next_batch))
+    db.commit()
+    
+    if data.get("rooms") and data["rooms"].get("join"):
+        for room_id, room in data["rooms"]["join"].items():
+
+            for event in room["state"]["events"]:
+                if event["type"] == "m.room.name":
+                    room_name = event["content"]["name"]
+
+                    # store room in db
+                    cursor.execute("INSERT OR IGNORE INTO rooms VALUES (?, ?)", (room_id, room_name))
+                    db.commit()
+            
+            for event in room["timeline"]["events"]:
+                if event["type"] == "m.room.message" and event["content"]["msgtype"] == "m.text":
+                    body = event["content"]["body"]
+                    sender = event["sender"]
+                    sender_display_name = sender[1:].split(":")[0]
+                    timestamp = event["origin_server_ts"]
+
+                    # store message in db
+                    cursor.execute("INSERT OR IGNORE INTO messages VALUES (?, ?, ?, ?, ?, ?)", (event["event_id"], room_id, sender, sender_display_name, timestamp, body))
+                    db.commit()
+            
+            # render room
+            render(next_batch, room_id)
+
+    return next_batch
+
+
+fetch_whoami()
+
+since = (cursor.execute("SELECT value FROM store WHERE key = 'next_batch'").fetchone() or (None,))[0]
+
+fetch_rooms(since)
+
+while True:
+    since = fetch_events(since)
+
+db.close()
